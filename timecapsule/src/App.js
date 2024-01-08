7ff70beb7be1b5c667b353cf48642d3aeacb574e
yurimn <leeyou6757@gmail.com>
2024-01-08 18:15:21 +0900

    Update App.js

--- timecapsule/src/App.js
+++ timecapsule/src/App.js
@@ -1,35 +1,29 @@
 import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SelectCapsule from "./pages/Capsule/SelectCapsule";
-import WriteCapsule from "./pages/Capsule/WriteCapsule";
 
 import Header from "./components/Layout/Header/Header";
 import Footer from "./components/Layout/Footer/Footer";
 
+import SelectCapsule from "./pages/Capsule/SelectCapsule";
+import WriteCapsule from "./pages/Capsule/WriteCapsule";
+
 function App() {
-<<<<<<< HEAD
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<SelectCapsule />} />
-					<Route path="/capsule" element={<SelectCapsule />} />
-					<Route path="/capsule/:id" element={<WriteCapsule />} />
-				</Routes>
-			</BrowserRouter>
+			<div className="App-container">
+				<Header />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<SelectCapsule />} />
+						<Route path="/capsule" element={<SelectCapsule />} />
+						<Route path="/capsule/:id" element={<WriteCapsule />} />
+					</Routes>
+				</BrowserRouter>
+				<Footer />
+			</div>
 		</div>
 	);
-=======
-  return (
-    <div className="App">
-      <div className="App-container">
-        <Header />
-
-        <Footer />
-      </div>
-    </div>
-  );
->>>>>>> main
 }
 
 export default App;
