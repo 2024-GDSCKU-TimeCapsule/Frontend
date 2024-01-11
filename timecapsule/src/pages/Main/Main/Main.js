2f7d7ef33e6d928f075d27c4dc29132aa360aa96
sk100 <sk100@korea.ac.kr>
2024-01-11 21:49:02 +0900

    [feat] Main 동그라미

--- timecapsule/src/pages/Main/Main/Main.js
+++ timecapsule/src/pages/Main/Main/Main.js
@@ -1,5 +1,19 @@
+import './Main.css';
+<style>import url('https://fonts.cdnfonts.com/css/post-no-bills-jaffna');</style>;
+
 const Main = () => {
-    return <div className="container">Main</div>;
+    return (
+        <div className="container">
+            <div className="dday-circle">
+                <div className="dday">D-235</div>
+                <div className="dday-text">Unsealed December 31, 2024</div>
+            </div>
+            <div className="menu">
+                <div className="create"></div>
+                <div className="read"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Main;
