04f0697ed34126ebd71479749a852f723aff81d2
sk100 <sk100@korea.ac.kr>
2024-01-12 23:15:56 +0900

    [feat] Main 페이지 요소, 원형  프로그레스바 전까지

--- timecapsule/src/pages/Main/Main/Main.js
+++ timecapsule/src/pages/Main/Main/Main.js
@@ -1,16 +1,30 @@
 import './Main.css';
+import menu from './img/Subtract.svg';
+import create from './img/Vector.svg';
+import mycapsule from './img/Union.svg';
+import archiving from './img/Group 22.svg';
 <style>import url('https://fonts.cdnfonts.com/css/post-no-bills-jaffna');</style>;
 
 const Main = () => {
+    const dday = 235;
     return (
         <div className="container">
             <div className="dday-circle">
-                <div className="dday">D-235</div>
-                <div className="dday-text">Unsealed December 31, 2024</div>
+                <div className="dday-progress">
+                    <div className="dday">D-{dday}</div>
+                    <div className="dday-text">Unsealed December 31, 2024</div>
+                </div>
             </div>
-            <div className="menu">
-                <div className="create"></div>
-                <div className="read"></div>
+
+            <div className="nav">
+                <div className="create">
+                    <img src={create} alt="create" />
+                </div>
+                <div className="menu">
+                    <img src={menu} alt="menu" />
+                    <img className="mycapsule" src={mycapsule} alt="mycapsule" />
+                    <img className="archiving" src={archiving} alt="archiving" />
+                </div>
             </div>
         </div>
     );
