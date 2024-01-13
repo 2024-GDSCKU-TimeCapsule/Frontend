cdcfdb067957a13838ed4064b2ace654425ff28a
yurimn <leeyou6757@gmail.com>
2024-01-14 06:04:35 +0900

    edit capsule

--- timecapsule/src/pages/Capsule/SelectCapsule.js
+++ timecapsule/src/pages/Capsule/SelectCapsule.js
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
-import { Pagination } from "swiper/modules";
+import Header from "../../components/Layout/Header/Header";
+import Footer from "../../components/Layout/Footer/Footer";
+
+import goal_capsule from "./images/goal_capsule.png";
+import memory_capsule from "./images/memory_capsule.png";
+import letter_capsule from "./images/letter_capsule.png";
 
 function SelectCapsule() {
 	const [capsule_name, setCapsuleName] = useState("");
@@ -23,21 +30,44 @@ function SelectCapsule() {
 
 	return (
 		<div className="select-capsule">
-			<Link to="/">Back to home</Link>
-			<h1>Select a capsule</h1>
+			<Header />
+			<div className="swiper-subtitle">1년 뒤 나에게 보내는 선물</div>
 			<Swiper
+				effect={"coverflow"}
+				grabCursor={true}
 				slidesPerView={2}
-				spaceBetween={30}
+				spaceBetween={0}
 				centeredSlides={true}
+				coverflowEffect={{
+					rotate: 0,
+					stretch: -20,
+					depth: 0,
+					modifier: 1,
+					slideShadows: false,
+				}}
 				pagination={{
 					clickable: true,
 				}}
-				modules={[Pagination]}
+				modules={[EffectCoverflow, Pagination]}
 				className="mySwiper"
 			>
-				<SwiperSlide>Slide 1</SwiperSlide>
-				<SwiperSlide>Slide 2</SwiperSlide>
-				<SwiperSlide>Slide 3</SwiperSlide>
+				{[memory_capsule, goal_capsule, letter_capsule].map(
+					(capsule, index) => (
+						<SwiperSlide key={index}>
+							<div className="swiper-title">
+								{
+									{
+										0: "MEMORY",
+										1: "GOAL",
+										2: "LETTER",
+									}[index]
+								}
+							</div>
+
+							<img src={capsule} alt="goal capsule" />
+						</SwiperSlide>
+					)
+				)}
 			</Swiper>
 			<form onSubmit={handleSubmit}>
 				<label htmlFor="capsule_name">Capsule name</label>
@@ -50,13 +80,15 @@ function SelectCapsule() {
 				/>
 				<button
 					type="submit"
+					className="make-capsule-button"
 					onClick={() => {
 						window.location.href = "/capsule/" + capsule_name;
 					}}
 				>
-					Submit
+					+ 타임캡슐 만들기
 				</button>
 			</form>
+			<Footer />
 		</div>
 	);
 }
