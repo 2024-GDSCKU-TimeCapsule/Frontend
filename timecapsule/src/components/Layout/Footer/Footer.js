9226b8fd4aab8d59c76794b6753e5bb3bd228088
yurimn <leeyou6757@gmail.com>
2024-01-14 17:07:03 +0900

    header & footer finish

--- timecapsule/src/components/Layout/Footer/Footer.js
+++ timecapsule/src/components/Layout/Footer/Footer.js
@@ -1,15 +1,33 @@
 import "./Footer.css";
-
 import React from "react";
 
-const Footer = () => {
+import { ReactComponent as Instagram } from "./instagram.svg";
+
+const Footer = ({ textColor = "#fff" }) => {
 	return (
-		<>
-			<div className="footer-container">
-				<div className="madeby">Made by GDSC Korea Univ.</div>
-				<div className="instagram">GDSC.koreauniv</div>
+		<div id="footer">
+			<div
+				id="MadeBy"
+				style={{
+					color: textColor,
+				}}
+			>
+				Made by GDSC Korea Univ.
 			</div>
-		</>
+			<div id="link">
+				<a
+					href="https://www.instagram.com/gdsc.koreauniv/"
+					id="insta-link"
+					style={{
+						color: textColor,
+					}}
+				>
+					<Instagram fill={textColor} />
+					<div style={{ width: "2px" }}></div>
+					GDSC.koreauniv
+				</a>
+			</div>
+		</div>
 	);
 };
 
