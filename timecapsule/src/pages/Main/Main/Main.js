f383c1a467e32e7d20150d7c32f1ab1ebbb286f3
sk100 <sk100@korea.ac.kr>
2024-01-14 17:21:40 +0900

    [feat] 페이지간 Link

--- timecapsule/src/pages/Main/Main/Main.js
+++ timecapsule/src/pages/Main/Main/Main.js
@@ -4,8 +4,10 @@ import create from './img/Vector.svg';
 import mycapsule from './img/Union.svg';
 import archiving from './img/Group 22.svg';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import dayjs from 'dayjs';
 import Header from '../../../components/Layout/Header/Header';
 import Footer from '../../../components/Layout/Footer/Footer';
+import { Link } from 'react-router-dom';
 
 const Main = () => {
     const dday = 235;
@@ -15,19 +17,25 @@ const Main = () => {
             <Header />
             <div className="dday-circle">
                 <CircularProgressbarWithChildren value={progress}>
-                    <div className="dday">D-{dday}</div>
+                    <div className="main-dday">D-{dday}</div>
                     <div className="dday-text">Unsealed December 31, 2024</div>
                 </CircularProgressbarWithChildren>
             </div>
 
             <div className="nav">
-                <div className="create">
-                    <img src={create} alt="create" />
-                </div>
+                <Link to="/capsule">
+                    <div className="create">
+                        <img src={create} alt="create" />
+                    </div>
+                </Link>
                 <div className="menu">
                     <img src={menu} alt="menu" />
-                    <img className="mycapsule" src={mycapsule} alt="mycapsule" />
-                    <img className="archiving" src={archiving} alt="archiving" />
+                    <Link to="/mycapsule">
+                        <img className="mycapsule" src={mycapsule} alt="mycapsule" />
+                    </Link>
+                    <Link to="/capsulearchiving">
+                        <img className="archiving" src={archiving} alt="archiving" />
+                    </Link>
                 </div>
             </div>
             <Footer />
