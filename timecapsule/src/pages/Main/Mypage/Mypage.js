3188db24b6e59fdbc0a31e306d4aeeaf0270e2aa
sk100 <sk100@korea.ac.kr>
2024-01-15 20:47:10 +0900

    [feat] 로그아웃 버튼 추가

--- timecapsule/src/pages/Main/Mypage/Mypage.js
+++ timecapsule/src/pages/Main/Mypage/Mypage.js
@@ -2,20 +2,55 @@ import './Mypage.css';
 import Header from '../../../components/Layout/Header/Header';
 import Footer from '../../../components/Layout/Footer/Footer';
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+// import { supabaseClient } from '../../../SupabaseProvider';
+
 const Mypage = () => {
     const nickname = 'nickname';
     const email = 'email@korea.ac.kr';
+    const [user, setUser] = useState(null);
+
+    // async function getUserData() {
+    //     await supabaseClient.auth.getUser().then(async (value) => {
+    //         if (value.data?.user) {
+    //             const { data: userData, error } = await supabaseClient
+    //                 .from('users')
+    //                 .select()
+    //                 .eq('user_id', value.data.user.id);
+    //             if (error) {
+    //                 console.log(error);
+    //             } else {
+    //                 setUser({
+    //                     id: userData[0].id,
+    //                     nickname: userData[0].nickname,
+    //                     userId: userData[0].user_id,
+    //                 });
+    //             }
+    //         }
+    //     });
+    // }
+
+    // useEffect(() => {
+    //     getUserData();
+    // }, []);
+
     return (
         <>
             <div className="main-container">
                 <Header />
-                <div class="nickname">{nickname} 님</div>
-                <div class="email">{email}</div>
-                <Link to="/nickname">
-                    <button class="button1 white">닉네임 수정하기</button>
-                </Link>
+                <div className="nickname">{nickname} 님</div>
+                <div className="email">{email}</div>
+                <div className="mypage-buttons">
+                    <Link to="/nickname">
+                        <button className="button1 white">닉네임 수정하기</button>
+                    </Link>
+                    <Link to="/login">
+                        <button className="button1 light">로그아웃</button>
+                    </Link>
+                </div>
+
                 <Link to="/withdraw">
-                    <div class="withdraw">계정 탈퇴</div>
+                    <div className="withdraw">계정 탈퇴</div>
                 </Link>
 
                 <div className="main-footer">
