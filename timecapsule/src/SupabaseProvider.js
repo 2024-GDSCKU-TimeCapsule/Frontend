35c7134a39c697f2be3612ae95932d88850963d5
sk100 <sk100@korea.ac.kr>
2024-01-17 08:52:33 +0900

    닉네임

--- timecapsule/src/SupabaseProvider.js
+++ timecapsule/src/SupabaseProvider.js
@@ -1,16 +1,12 @@
-import React from "react";
-import { createClient } from "@supabase/supabase-js";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import React from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 const SupabaseProvider = ({ children }) => {
-	const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-	const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-	const supabaseClient = createClient(supabaseUrl, supabaseKey);
-	return (
-		<SessionContextProvider supabaseClient={supabaseClient}>
-			{children}
-		</SessionContextProvider>
-	);
+    const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+    const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+    const supabaseClient = createClient(supabaseUrl, supabaseKey);
+    return <SessionContextProvider supabaseClient={supabaseClient}>{children}</SessionContextProvider>;
 };
 
 export default SupabaseProvider;
