48020061fd23f7a847201d448de67820904e98e3
sk100 <sk100@korea.ac.kr>
2024-01-16 18:52:24 +0900

    merge 오류 해결

--- timecapsule/src/pages/Main/Mypage/Mypage.js
+++ timecapsule/src/pages/Main/Mypage/Mypage.js
@@ -1,52 +1,82 @@
 import './Mypage.css';
 import Header from '../../../components/Layout/Header/Header';
 import Footer from '../../../components/Layout/Footer/Footer';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-// import { supabaseClient } from '../../../SupabaseProvider';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 const Mypage = () => {
     const nickname = 'nickname';
     const email = 'email@korea.ac.kr';
-    const [user, setUser] = useState(null);
+    const supabaseClient = useSupabaseClient();
+    const navigate = useNavigate();
+    // const [user, setUser] = useState(null);
+    const [userID, setUserID] = useState({});
+    const [user, setUser] = useState({
+        id: '',
+        nickname: '',
+        userId: '',
+    });
 
-    // async function getUserData() {
-    //     await supabaseClient.auth.getUser().then(async (value) => {
-    //         if (value.data?.user) {
-    //             const { data: userData, error } = await supabaseClient
-    //                 .from('users')
-    //                 .select()
-    //                 .eq('user_id', value.data.user.id);
-    //             if (error) {
-    //                 console.log(error);
-    //             } else {
-    //                 setUser({
-    //                     id: userData[0].id,
-    //                     nickname: userData[0].nickname,
-    //                     userId: userData[0].user_id,
-    //                 });
-    //             }
-    //         }
-    //     });
-    // }
+    useEffect(() => {
+        async function checkLogin() {
+            const authInfo = await supabaseClient.auth.getSession();
+            const session = authInfo.data.session;
 
-    // useEffect(() => {
-    //     getUserData();
-    // }, []);
+            if (session == null) {
+                console.log('log in fail');
+                navigate('/login');
+            } else {
+                console.log('log in success');
+            }
+        }
+        checkLogin();
+
+        async function getUserData() {
+            await supabaseClient.auth.getUser().then(async (value) => {
+                if (value.data?.user) {
+                    const { data: userData, error } = await supabaseClient
+                        .from('users')
+                        .select()
+                        .eq('user_id', value.data.user.id);
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        setUserID(value.data.user.id);
+                    }
+                }
+            });
+        }
+        getUserData();
+    }, [supabaseClient]);
+
+    async function signOut() {
+        try {
+            const { error } = await supabaseClient.auth.signOut();
+            if (error) {
+                console.error('Sign-out error:', error.message);
+            } else {
+                console.log('Sign-out successful');
+                navigate('/login'); /* 로그인창으로 */
+            }
+        } catch (error) {
+            console.error('Unexpected error during sign-out:', error);
+        }
+    }
 
     return (
         <>
             <div className="main-container">
                 <Header />
-                <div className="nickname">{nickname} 님</div>
+                <div className="nickname">{user.nickname} 님</div>
                 <div className="email">{email}</div>
                 <div className="mypage-buttons">
                     <Link to="/nickname">
                         <button className="button1 white">닉네임 수정하기</button>
                     </Link>
-                    <Link to="/login">
-                        <button className="button1 light">로그아웃</button>
-                    </Link>
+                    <button className="button1 light" onClick={signOut}>
+                        로그아웃
+                    </button>
                 </div>
 
                 <Link to="/withdraw">
