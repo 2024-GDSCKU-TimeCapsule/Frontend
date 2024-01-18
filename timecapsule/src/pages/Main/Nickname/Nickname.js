0132ba18625903faaf6c6aadda6749e4a9491e9d
sk100 <sk100@korea.ac.kr>
2024-01-18 23:59:05 +0900

    [style] 타임캡슐 만들기 폰트

--- timecapsule/src/pages/Main/Nickname/Nickname.js
+++ timecapsule/src/pages/Main/Nickname/Nickname.js
@@ -1,144 +1,139 @@
-import "./Nickname.css";
-import Header from "../../../components/Layout/Header/Header";
-import Footer from "../../../components/Layout/Footer/Footer";
-import { useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import './Nickname.css';
+import Header from '../../../components/Layout/Header/Header';
+import Footer from '../../../components/Layout/Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 const Nickname = () => {
-  const supabaseClient = useSupabaseClient();
-  const navigate = useNavigate();
-  const [user, setUser] = useState({
-    id: "",
-    nickname: "",
-    userId: "",
-  });
-  const [newUserNickname, setNewUserNickname] = useState("");
-  const [currentNickname, setCurrentNickname] = useState("");
-  useEffect(() => {
-    async function checkLogin() {
-      const authInfo = await supabaseClient.auth.getSession();
-      const session = authInfo.data.session;
+    const supabaseClient = useSupabaseClient();
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+        id: '',
+        nickname: '',
+        userId: '',
+    });
+    const [newUserNickname, setNewUserNickname] = useState('');
+    const [currentNickname, setCurrentNickname] = useState('');
+    useEffect(() => {
+        async function checkLogin() {
+            const authInfo = await supabaseClient.auth.getSession();
+            const session = authInfo.data.session;
 
-      if (session == null) {
-        console.log("log in fail");
-        navigate("/login");
-      } else {
-        console.log("log in success");
-      }
-    }
-    checkLogin();
-
-    async function getUserData() {
-      await supabaseClient.auth.getUser().then(async (value) => {
-        if (value.data?.user) {
-          const { data: userData, error } = await supabaseClient
-            .from("users")
-            .select()
-            .eq("user_id", value.data.user.id);
-
-          // console.log('Supabase Data:', userData);
-          // console.log(value.data.user);
-          if (error) {
-            console.log(error);
-          } else {
-            setUser({
-              id: userData[0].id,
-              nickname: userData[0].nickname,
-              userId: userData[0].user_id,
-            });
-            setCurrentNickname(userData[0].nickname); // 현재 닉네임 설정
-          }
+            if (session == null) {
+                console.log('log in fail');
+                navigate('/login');
+            } else {
+                console.log('log in success');
+            }
         }
-      });
+        checkLogin();
+
+        async function getUserData() {
+            await supabaseClient.auth.getUser().then(async (value) => {
+                if (value.data?.user) {
+                    const { data: userData, error } = await supabaseClient
+                        .from('users')
+                        .select()
+                        .eq('user_id', value.data.user.id);
+
+                    // console.log('Supabase Data:', userData);
+                    // console.log(value.data.user);
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        setUser({
+                            id: userData[0].id,
+                            nickname: userData[0].nickname,
+                            userId: userData[0].user_id,
+                        });
+                        setCurrentNickname(userData[0].nickname); // 현재 닉네임 설정
+                    }
+                }
+            });
+        }
+        getUserData();
+    }, [supabaseClient]);
+    async function updateNickname() {
+        // console.log('여기', newUserNickname);
+        const { error } = await supabaseClient
+            .from('users')
+            .update({
+                nickname: newUserNickname,
+            })
+            .eq('user_id', user.userId);
+
+        if (error) {
+            console.error('Error updating nickname:', error.message);
+        } else {
+            console.log('Nickname updated successfully');
+            //setUser({ ...user, nickname: "update test" });
+        }
     }
-    getUserData();
-  }, [supabaseClient]);
-  async function updateNickname() {
-    // console.log('여기', newUserNickname);
-    const { error } = await supabaseClient
-      .from("users")
-      .update({
-        nickname: newUserNickname,
-      })
-      .eq("user_id", user.userId);
 
-    if (error) {
-      console.error("Error updating nickname:", error.message);
-    } else {
-      console.log("Nickname updated successfully");
-      //setUser({ ...user, nickname: "update test" });
+    const handleNicknameChange = (e) => {
+        setNewUserNickname(e.target.value); // 새로운 닉네임 업데이트
+    };
+
+    async function updateNickname(nickname) {
+        const { error } = await supabaseClient
+            .from('users')
+            .update({
+                nickname: nickname,
+            })
+            .eq('user_id', user.userId);
+
+        if (error) {
+            console.error('Error updating nickname:', error.message);
+        } else {
+            console.log('Nickname updated successfully');
+            // 다른 처리가 필요하면 여기에 추가
+        }
     }
-  }
 
-  const handleNicknameChange = (e) => {
-    setNewUserNickname(e.target.value); // 새로운 닉네임 업데이트
-  };
+    const handleSubmit = () => {
+        const nicknameToUpdate = newUserNickname.trim() === '' ? currentNickname : newUserNickname;
+        updateNickname(nicknameToUpdate);
+        navigate('/mypage');
 
-  async function updateNickname(nickname) {
-    const { error } = await supabaseClient
-      .from("users")
-      .update({
-        nickname: nickname,
-      })
-      .eq("user_id", user.userId);
+        // 필요한 경우 추가 처리
+    };
 
-    if (error) {
-      console.error("Error updating nickname:", error.message);
-    } else {
-      console.log("Nickname updated successfully");
-      // 다른 처리가 필요하면 여기에 추가
-    }
-  }
-
-  const handleSubmit = () => {
-    const nicknameToUpdate =
-      newUserNickname.trim() === "" ? currentNickname : newUserNickname;
-    updateNickname(nicknameToUpdate);
-    navigate("/mypage");
-
-    // 필요한 경우 추가 처리
-  };
-
-  return (
-    <>
-      <div className="main-container">
-        <Header />
-        <div className="main-modal">
-          <div className="title">닉네임설정</div>
-          <div>
-            <input
-              type="text"
-              value={newUserNickname} // 닉네임 입력 값으로 설정
-              onChange={handleNicknameChange}
-              placeholder={currentNickname} // placeholder에 현재 닉네임 설정
-            ></input>
-          </div>
-          <div className="buttons">
-            <button
-              type="submit"
-              className="button2 white"
-              onClick={handleSubmit}
-            >
-              확인
-            </button>
-            <button
-              type="button"
-              className="button2"
-              onClick={() => {
-                window.location.href = "/mypage";
-              }}
-            >
-              취소
-            </button>
-          </div>
-        </div>
-        {/* <div class="withdraw">계정 탈퇴</div> */}
-        <div className="main-footer">
-          <Footer />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="main-container">
+                <Header />
+                <div className="main-modal">
+                    <div className="title">닉네임 설정</div>
+                    <div>
+                        <input
+                            type="text"
+                            value={newUserNickname} // 닉네임 입력 값으로 설정
+                            onChange={handleNicknameChange}
+                            placeholder={currentNickname} // placeholder에 현재 닉네임 설정
+                        ></input>
+                    </div>
+                    <div className="buttons">
+                        <button type="submit" className="button2 white" onClick={handleSubmit}>
+                            확인
+                        </button>
+                        <button
+                            type="button"
+                            className="button2"
+                            onClick={() => {
+                                window.location.href = '/mypage';
+                            }}
+                        >
+                            취소
+                        </button>
+                    </div>
+                </div>
+                {/* <div class="withdraw">계정 탈퇴</div> */}
+                <div className="main-footer">
+                    <Footer />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Nickname;
