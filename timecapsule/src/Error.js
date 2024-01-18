675ff1cea5897916a191eb32d90110cecb2fb0c3
yurimn <leeyou6757@gmail.com>
2024-01-19 02:18:23 +0900

    error 페이지 수정

--- timecapsule/src/Error.js
+++ timecapsule/src/Error.js
@@ -1,6 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Error({ code, message }) {
+	const navigate = useNavigate();
+	const [time, setTime] = useState(3);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setTime((prevTime) => {
+				if (prevTime === 1) {
+					clearInterval(timer);
+					navigate("/");
+				}
+				return prevTime - 1;
+			});
+		}, 1000);
+
+		return () => clearInterval(timer);
+	}, [navigate]);
 	return (
 		<div className="error">
+			<h1> Error</h1>
+			<h2> {time} 초 후 "/"로 돌아갑니다..</h2>
 			<h1>{code}</h1>
 			<h2>{message}</h2>
 		</div>
